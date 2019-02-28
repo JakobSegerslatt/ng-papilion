310c512591b03feac3852b4876de6b7052c9a9b4
Jakob Segerslätt <segerslatt@gmail.com>
2019-02-28 12:44:56 +0100

    commented code

--- projects/ng-papilion/src/lib/pipes/capitalize.pipe.ts
+++ projects/ng-papilion/src/lib/pipes/capitalize.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 /**
  * Converts a string to captilized by setting the first char to uppercase
  */
+@Pipe({ name: 'capitalize' })
 export class CapitalizePipe implements PipeTransform {
   transform(value: any) {
     if (typeof value === 'string' && value[0] !== null) {
